1123dafc1456d1cc8cdc44e4d91dc61a7a083f6c
bang26112001 <bang26112001@gmail.com>
2021-03-28 22:10:35 +0700

    bang lam xong admin

---
+++ component/Admin_main.php
@@ -0,0 +1,291 @@
+<div class="main-content">
+                <!-- Header -->
+        <header>
+            <h1>
+                <label for="nav-toggle">
+                    <span class="las la-bars"></span>
+                </label>
+                Dashboard
+            </h1>
+
+            <div class="search-wrapper">
+                <span class="las la-search"></span>
+                <input type="search" placeholder="Search here"/>
+            </div>
+
+            <div class="user-wrapper">
+                <img src="../img/profile1.jpg" width="30px" height="30px" alt="">
+                <div>
+                    <h4>Duc Bang</h4>
+                    <small>Super admin</small>
+                </div>
+            </div>
+        </header>
+
+        <main>
+
+            <div class="cards">
+                <div class="card-single">
+                    <div>
+                        <h1>54</h1>
+                        <span>Customers</span>
+                    </div>
+                    <div>
+                        <span class="las la-users"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>25</h1>
+                        <span>Projects</span>
+                    </div>
+                    <div>
+                        <span class="las la-clipboard-list"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>100</h1>
+                        <span>Orders</span>
+                    </div>
+                    <div>
+                        <span class="las la-shopping-bag"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>$6k</h1>
+                        <span>Income</span>
+                    </div>
+                    <div>
+                        <span class="lab la-google-wallet"></span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="recent-grid">
+                <div class="projects">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>Recent Projects</h3>
+                            <button>See All <span class="las la-arorw-right"></span></button>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table width="100%">
+                                    <thead>
+                                        <tr>
+                                            <td>Project Title</td>
+                                            <td>Department</td>
+                                            <td>Status</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>UI/UX Designer</td>
+                                            <td>UI team</td>
+                                            <td><span class="status purple"></span>
+                                                Review
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Web development</td>
+                                            <td>Frontend</td>
+                                            <td><span class="status pink"></span>
+                                                in progress
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ushop App</td>
+                                            <td>Mobile team</td>
+                                            <td><span class="status orange"></span>
+                                                pending
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>UI/UX Designer</td>
+                                            <td>UI team</td>
+                                            <td><span class="status purple"></span>
+                                                Review
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Web development</td>
+                                            <td>Frontend</td>
+                                            <td><span class="status pink"></span>
+                                                in progress
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ushop App</td>
+                                            <td>Mobile team</td>
+                                            <td><span class="status orange"></span>
+                                                pending
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>UI/UX Designer</td>
+                                            <td>UI team</td>
+                                            <td><span class="status purple"></span>
+                                                Review
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Web development</td>
+                                            <td>Frontend</td>
+                                            <td><span class="status pink"></span>
+                                                in progress
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ushop App</td>
+                                            <td>Mobile team</td>
+                                            <td><span class="status orange"></span>
+                                                pending
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>UI/UX Designer</td>
+                                            <td>UI team</td>
+                                            <td><span class="status purple"></span>
+                                                Review
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Web development</td>
+                                            <td>Frontend</td>
+                                            <td><span class="status pink"></span>
+                                                in progress
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ushop App</td>
+                                            <td>Mobile team</td>
+                                            <td><span class="status orange"></span>
+                                                pending
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                 
+                <div class="custommers">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>New Customer</h3>
+                            <button>See All <span class="las la-arrow-right"></span> </button>
+                        </div>
+                        <div class="card-body">
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="../img/profile2.jpg" width="40px" height="40px" alt="">
+                                    <div>
+                                        <h4>Thanh Man</h4>
+                                        <small>CEO VinGroup</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+
+    </div>
